0ce3bc6b6fd735e5962fa22aa6c56dd8dfe481dc
limjisuu <dlawltn1999@naver.com>
2020-09-30 16:47:04 +0900

    2741 N 찍기

---
+++ stages/03. For/2741.c
@@ -0,0 +1,11 @@
+# define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+
+int main(void) {
+	int num;
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++) {
+		printf("%d\n", i + 1);
+	}
+	return 0;
+}
