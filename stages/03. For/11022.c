6eee9f72c4c133d50111e5f8129467eaf2f40e66
limjisuu <dlawltn1999@naver.com>
2020-09-30 17:37:42 +0900

    11022 A+B - 8

---
+++ stages/03. For/11022.c
@@ -0,0 +1,12 @@
+# define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+
+int main(void) {
+	int num, a, b;
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++) {
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d + %d = %d\n", i + 1, a, b, a + b);
+	}
+	return 0;
+}
